2201f0fc6fd8c90eb18e1267d98ca9c23690d6e8
Matous Polauf <polauf@post.cz>
2022-09-06 13:16:56 +0200

    Lets make a version.

--- WebCamEnhancer/constants.py
+++ WebCamEnhancer/constants.py
@@ -6,7 +6,7 @@ from appdirs import user_data_dir
 APP_NAME = "WebCam Enhancer"
 APP_AUTHOR = "Matouš Polauf"
 APP_AUTHOR_SHORT = "MPStuff"
-APP_VERSION = "0.5.0"
+APP_VERSION = "0.5.1"
 
 CONFIG_DIR = Path(user_data_dir(APP_NAME, APP_AUTHOR_SHORT))
 BASE_CONFIG = CONFIG_DIR / "config.json"
