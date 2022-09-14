a1ef5708e57bc2756e434cca5654b6a9923d708e
Matous Polauf <polauf@post.cz>
2022-09-14 03:00:47 +0200

    Fixed problems with function 'draw on image' to be able to hadle overflows and draw by center.

--- WebCamEnhancer/core/utils.py
+++ WebCamEnhancer/core/utils.py
@@ -56,6 +56,38 @@ def configure_logging(log_file, level):
 logger = logging.getLogger(APP_NAME)
 
 
+def resolve_xy_center(top_shape, bottom_shape, xy=None, center=None):
+    x1, y1 = 0,0
+    h1, w1 = top_shape
+    h, w = bottom_shape
+    if xy is not None:
+        x, y = xy
+    elif center is not None:
+        cx, cy = center
+        x = int(cx - w1/2.)
+        if 0>x:
+            x1 = -x
+            w = w1 + x
+            x = 0  
+        else:
+            if (cx + w1/2) >= w:
+                w1 = w - x
+            w = w1 
+
+        y = int(cy - h1/2.)
+        if 0 > y:
+            y1 = -y
+            h = h1 + y
+            y = 0
+        else:
+            if (cy + h1/2) >= h:
+                h1 = h - y
+            h = h1
+
+    else:
+        raise ValueError("Need 'xy' or 'center' keyword argument.")
+    return (x, y, w,h),(x1,y1,w1,h1)
+
 def rotate_image(image: np.array, angle: float) -> np.array:
   return cv2.warpAffine(
       image,
@@ -68,26 +100,15 @@ def rotate_image(image: np.array, angle: float) -> np.array:
         flags=cv2.INTER_LINEAR
         )
 
-def draw_on_image(bottom: np.array, top: np.array, x=0, y=0):
-    (h, w) = top.shape[:2]
-    y1, y2 = y, y + h
-    x1, x2 = x, x + w
-    
-    x_lim = 0
-    y_lim = 0
-    if x2 >= bottom.shape[1]:
-        x_lim = x2 - bottom.shape[1] + 1
-        w -= x_lim
-        x2 -= x_lim
-    if y2 >= bottom.shape[0]:
-        y_lim = y2 - bottom.shape[0] + 1
-        h -= y_lim
-        y2 -= y_lim
-
-    alpha_top = top[:h, :w, 3] / 255.0
+def draw_on_image(bottom: np.array, top: np.array, xy=None, center=None,transparency=0):
+    ((x, y, w, h), (bx, by, bw, bh)) = resolve_xy_center(top.shape[:2],bottom.shape[:2], xy, center)
+    alpha_top = top[by:by+bh, bx:bx+bw, 3] / 255.0
     alpha_bottom = 1.0 - alpha_top
-    for c in range(0, 3):
-        bottom[y1:y2, x1:x2, c] = (alpha_top * top[:h, :w, c] +
-                                   alpha_bottom * bottom[y1:y2, x1:x2, c])
+    for c in range(3):
+        # cv2.rectangle(bottom,(x,y), (x+w, y+h), (0,255,0),2)
+        bottom[y:y+h, x:x+w, c] = (
+            alpha_top * top[by:by+bh, bx:bx+bw, c] +
+            alpha_bottom * bottom[y:y+h, x:x+w, c]
+            )
     if bottom.shape[2] == 4:
-        bottom[y1:y2, x1:x2, 3] = np.maximum(top[:h, :w, 3], bottom[y1:y2, x1:x2, 3])
+        bottom[y:y+h, x:x+w, 3] = np.maximum(top[by:by+bh, bx:bx+bw, 3], bottom[y:y+h, x:x+w, 3])*(1.-transparency/255.)
